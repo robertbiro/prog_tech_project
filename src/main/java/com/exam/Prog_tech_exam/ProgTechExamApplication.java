9d40ef83a5ffe998a68230ccba34b5dccd1e260d
robertbiro <biro.k.robert@gmail.com>
2023-11-14 09:10:19 +0100

    init Spring

---
+++ src/main/java/com/exam/Prog_tech_exam/ProgTechExamApplication.java
@@ -0,0 +1,13 @@
+package com.exam.Prog_tech_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgTechExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgTechExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/Prog_tech_exam/ProgTechExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.Prog_tech_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgTechExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
